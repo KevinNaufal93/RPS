4ed581393c57d5e06db7bb9377f28cf5588cbdec
erwanrendy <88603224+erwanrendy@users.noreply.github.com>
2021-10-28 19:04:28 +0700

    test-const
    1. plus const a & b jumlah jadi c
    2. hapus command di index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use('/assets', express.static('assets')); //naming public as assets isnt okay, change later
-app.use('/scripts', express.static('scripts')); //naming public as assets isnt okay, change later
+app.use("/assets", express.static("assets"));
+app.use("/scripts", express.static("scripts")); //naming public as assets isnt okay, change later
 
-
-app.get('/', (req,res) => {
-    res.render('frontpage')
+app.get("/", (req, res) => {
+  res.render("frontpage");
 });
 
-app.get('/play', (req,res) => {
-    res.render('mainPage')
+app.get("/play", (req, res) => {
+  res.render("mainPage");
 });
 
-app.listen(port, console.log("server running on port " + port)) 
+app.listen(port, console.log("server running on port " + port));
