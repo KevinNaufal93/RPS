42f3da1531927c62a0a3292be1f451d6a6d3fc83
MorningToast21 <Kevinnp28@gmail.com>
2021-10-28 15:37:18 +0700

    Master
    -Reformat everything to use with view engine

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('assets')); //naming public as assets isnt okay, change later
+
+app.get('/', (req,res) => {
+    res.render('frontpage')
+});
+
+app.get('/play', (req,res) => {
+    res.render('mainPage')
+});
+
+app.listen(port, console.log("server running on port " + port)) 
